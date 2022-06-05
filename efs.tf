8e789bbb5ca4dd4d42cd642cadb5101368eb167e
jengm22 <mahtarrjeng@gmail.com>
2022-06-05 22:06:27 +0100

    Update efs.tf

--- efs.tf
+++ efs.tf
@@ -18,21 +18,3 @@ resource "aws_efs_mount_target" "efs_mount" {
 }
 
 # # Creating Mount Point for EFS
-# resource "null_resource" "connection" {
-#   depends_on = [aws_efs_mount_target.efs_mount, ]
-#   connection {
-#     type        = "ssh"
-#     user        = "ec2-user"
-#     private_key = tls_private_key.p_key.private_key_pem
-#     # host        = aws_instance.task2-instance.public_ip
-#   }
-
-#   // Mounting the EFS on the folder /var/www/html and pulling the code from github
-#   provisioner "remote-exec" {
-#     inline = [
-#       "sudo echo ${aws_efs_file_system.iac-efs.dns_name}:/var/www/html efs defaults,_netdev 0 0 >> sudo /etc/fstab",
-#       "sudo mount  ${aws_efs_file_system.iac-efs.dns_name}:/  /var/www/html",
-#       # "sudo git clone https://github.com/Priyanshi541/HTask2.git /var/www/html/",
-#     ]
-#   }
-# }
